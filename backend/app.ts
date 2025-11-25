4f3a67850567d9094150fa8d812376317d65ce5e
CESAR CHAVES <cesarchaves@MacBook-Pro-de-CESAR.local>
2025-11-25 16:01:59 -0500

    chore: expand allowed origins

--- backend/app.ts
+++ backend/app.ts
@@ -12,6 +12,8 @@ const allowedOrigins = [
   "https://www.rankomotenashi.com",
   "https://rankomotenashi.com",
   "http://localhost:5173",
+  "http://localhost:3000",
+  "https://omotenashi-vite.vercel.app",
 ];
 
 app.use(
